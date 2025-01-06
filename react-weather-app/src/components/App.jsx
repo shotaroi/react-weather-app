cf3b6a3ab15c852499e99ba49f1a36f90e9e88f5
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-06 17:39:45 +0100

    Add form

--- react-weather-app/src/components/App.jsx
+++ react-weather-app/src/components/App.jsx
@@ -2,12 +2,24 @@ import { useState } from 'react'
 import '../index.css'
 
 function App() {
+  const [ city, setCity ] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(city);
+  }
 
   return (
     <>
       <div>
         <h1>React Weather App</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+            placeholder='Enter city name'
+            value={city} 
+            onChange={(e) => {setCity(e.target.value)}}/>
+          <button type='submit'>Get Weather</button>
+        </form>
       </div>
     </>
   )
